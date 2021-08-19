c74414c6601a912ceab26fb2b1d52d7c65b8c87f
Rasagna0409 <77762529+Rasagna0409@users.noreply.github.com>
2021-08-19 16:36:48 -0500

    Updated code

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var a=10;
+var b=20;
+
+var sum=0;
+
+sum = a+b;
+print(sum);
+
+var a1 = 20.0;
+var a2 = 30.0;
+var product = a1*a2;
+print(product)
